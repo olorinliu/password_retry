ca40eed8ff3a56bed6dc836b5ee0c51745d39193
Dominic Liu <olorinliu@gmail.com>
2020-08-26 16:41:28 +0800

    basic version done

---
+++ password_retry.py
@@ -0,0 +1,17 @@
+password = "a123456"
+i = 3 #剩餘機會
+while True:
+	password_key = input("請輸入密碼： ")
+	if password_key == password:
+		print("登入成功！") 
+		break # 逃出迴圈
+	else:
+		i = i - 1
+		print("密碼錯誤！ 還有", i, "次機會")
+		if i == 0:
+			break
+                					
+
+
+
+
